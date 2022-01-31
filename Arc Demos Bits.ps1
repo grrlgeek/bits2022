7595aba7194a2001fb325f9bc694601fa97d32a8
Ben Weissman <b.weissman@solisyon.de>
2022-01-31 15:51:14 +0000

    changes

--- Arc Demos Bits.ps1
+++ Arc Demos Bits.ps1
@@ -12,8 +12,6 @@ kubectl get nodes
 # Let's stay with our "big" cluster for now
 kubectl config use-context kubeadm-big
 kubectl get nodes
-kubectl get nodes -o json
-
 
 # A bit time factor is image download - so we've pre-pulled them
 kubectl get nodes (kubectl get nodes -o jsonpath="{.items[1].metadata.name}" ) -o jsonpath="{range .status.images[*]}{.names[1]}{'\n'}{end}" | grep arcdata 
